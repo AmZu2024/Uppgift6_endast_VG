83e731f1ddc52c965c7205cb60db5ac08cfcdf35
AmZu2024 <amar.zukanovic57@gmail.com>
2024-10-01 10:06:28 +0200

    Skapat en klass för att underlätta och strukturera programmet med olika metoder.
    Metoder som har skapats är: Metod för writing out names
    
    Metod för att lägga till namn i listan .

--- Program.cs
+++ Program.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 namespace NameSorter
 {
+    public class Metoder
+    {
+        List<string> names = new List<string> { "Anna", "John", "Alice", "Bo" };
+       
+        //Metod for writing out names:
+        public void lista()
+        {
+            foreach (string name in names)
+            {
+                 Console.WriteLine(name);
+            }
+        }
+        //Metod för att lägga till namn i listan 
+        public void lägga_till()
+        {
+            string addname = Console.ReadLine();
+            names.Add(addname);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
 
+            Metoder metoder = new Metoder();
+            metoder.lägga_till();
+            metoder.lista();
+            /*
             List<string> names = new List<string> { "Anna", "John", "Alice", "Bo" };
             Console.WriteLine("Original List");
 
@@ -14,16 +38,16 @@ namespace NameSorter
             {
                 Console.WriteLine(name);
             }
-            
+
             names.Sort();
             Console.WriteLine("\nSorted list");
 
-            foreach(var name in names)
+            foreach (var name in names)
             {
                 Console.WriteLine(name);
             }
             Console.WriteLine("\nEnter name to search:");
-            string searchName= Console.ReadLine();
+            string searchName = Console.ReadLine();
             if (names.Contains(searchName))
             {
                 Console.WriteLine($"{searchName} is in the list.");
@@ -33,7 +57,7 @@ namespace NameSorter
                 Console.WriteLine($"{searchName} is not in the list.");
             }
 
-            
+            */
         }
     }
 }
